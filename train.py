3270d86a768cea62caedbc51b97b15737a01cb70
MonicaSai7 <monica.sai7@gmail.com>
2021-01-27 08:37:49 +0530

    Update data prep

--- train.py
+++ train.py
@@ -9,20 +9,14 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Dataset, Datastore
+from azureml.data.datapath import DataPath
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+url_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+ds = TabularDatasetFactory.from_delimited_files(url_path)
 
 def clean_data(data):
     # Dict for cleaning data
@@ -50,6 +44,14 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df,y_df
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.80,test_size=0.20, random_state=42)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
@@ -66,6 +68,8 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
+    os.makedirs('outputs',exist_ok = True)
+    joblib.dump(model,'outputs/model.joblib')
     run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
